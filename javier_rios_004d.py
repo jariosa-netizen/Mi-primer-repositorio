2a1ae8df9e413e7e16c680044b3269024d580a00
jariosa-netizen <ja.riosa@duocuc.cl>
2026-07-09 18:04:26 -0400

    realizado dia 09-07-2026 16:00 hasta 18:03

---
+++ javier_rios_004d.py
@@ -0,0 +1,13 @@
+def leer_opcion():
+
+
+
+
+
+
+
+
+def menu():
+    print("========== MENU PRINCIPAL ===========\n1. cupos por genero\n2. busqueda  de peliculas por rango de precio")
+    print("4 Agregar peliculas\n5. Eliminar pelicula\n6. salir\=============")
+
